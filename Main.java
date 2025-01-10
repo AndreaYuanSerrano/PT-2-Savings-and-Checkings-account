ec544b1adb629a8e17bad99a238b0c6c39884db1
AndreaYuanSerrano <andreayuanserrano@gmail.com>
2025-01-10 11:10:06 +0800

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,24 +3,25 @@ public class Main {
         SavingsAccount savingsAccount = new SavingsAccount("Mahal ka ba nya?", "Andrea", 1000.00, 0.05);
 
   System.out.println("Depositing $500 into Savings Account...");
-        savingsAccount.deposit(500.00);
+  savingsAccount.deposit(500.00);
   System.out.println("New balance: " + savingsAccount.getBalance()); 
 
    System.out.println("Withdrawing $200 from Savings Account...");
-        savingsAccount.withdraw(200.00);
+   savingsAccount.withdraw(200.00);
   System.out.println("New balance: " + savingsAccount.getBalance());
 
-       CheckingsAccount checkingsAccount = new CheckingsAccount("CA123", "Jane Doe", 500.00, 200.00);
+   CheckingsAccount checkingsAccount = new CheckingsAccount("CA123", "Jane Doe", 500.00, 200.00);
   System.out.println("Depositing $300 into Checking Account...");
-        checkingsAccount.deposit(300.00);
+  checkingsAccount.deposit(300.00);
    System.out.println("New balance: " + checkingsAccount.getBalance()); 
 
  System.out.println("Withdrawing $600 from Checking Account (within overdraft limit)...");
-        checkingsAccount.withdraw(600.00);
+checkingsAccount.withdraw(600.00);
   System.out.println("New balance: " + checkingsAccount.getBalance());
 
   System.out.println("Withdrawing $800 from Checking Account (exceeds overdraft limit)...");
-        checkingsAccount.withdraw(800.00);
+ checkingsAccount.withdraw(800.00);
   System.out.println("New balance: " + checkingsAccount.getBalance());
     }
 }
+
